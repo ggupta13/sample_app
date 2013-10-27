bc7c412c47d71f1d3559f0c8716fbd59860d18a6
ggupta13 <gagan.aj.gupta@gmail.com>
2013-10-27 16:21:11 -0400

    Finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,5 +6,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def name(user)
+    user.name
+  end
   
 end
